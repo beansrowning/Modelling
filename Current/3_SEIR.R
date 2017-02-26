b68ff75902cb914873a3323c957adf5e2126fadf
beansrowning <sean.browning94@gmail.com>
2017-02-26 15:58:09 +0000

    The little ones grow up now
    Those in the young population division move into the old population
    division at a rate equal to the inverse of the size of the compartment
    in days.

--- Current/3_SEIR.R
+++ Current/3_SEIR.R
@@ -11,17 +11,17 @@ library(googleVis)
 init.values = c(
   S1 = c(10^4,10^5),
   E1 = c(0,0),
-  I1 = c(20,2),
+  I1 = c(10,2),
   R1 = c(0,0),
   S2 = c(10^4,10^5),
   E2 = c(0,0),
-  I2 = c(20,2),
+  I2 = c(10,2),
   R2 = c(0,0),
   S3 = c(10^4,10^5),
   E3 = c(0,0),
-  I3 = c(20,2),
+  I3 = c(10,2),
   R3 = c(0,0),
-  D = c(0,0,0) #Begining of vaccination
+  D = c(30,100,0) #Begining of vaccination
   )
 
 transitions = list(
@@ -32,10 +32,10 @@ transitions = list(
   c(I11 = -1, R11 = +1),  #Recovery
   c(S11 = +1), #Vaccinations
   c(R11 = +1),
-  c(S11 = -1), #Deaths
-  c(E11 = -1),
-  c(I11 = -1),
-  c(R11 = -1),
+  c(S11 = -1, S12 = +1), #aging
+  c(E11 = -1, E12 = +1),
+  c(I11 = -1, I12 = +1),
+  c(R11 = -1, R12 = +1),
   #old
   c(S12 = -1, E12 = +1), #Infection
   c(E12 = -1, I12 = +1), #infectious
@@ -51,10 +51,10 @@ transitions = list(
   c(I21 = -1, R21 = +1), #Recovery
   c(S21 = +1), #Vaccinations
   c(R21 = +1),
-  c(S21 = -1), #Deaths
-  c(E21 = -1),
-  c(I21 = -1),
-  c(R21 = -1),
+  c(S21 = -1, S22 = +1), #aging
+  c(E21 = -1, E22 = +1),
+  c(I21 = -1, I22 = +1),
+  c(R21 = -1, R22 = +1),
   #old
   c(S22 = -1, E22 = +1), #Infection
   c(E22 = -1, I22 = +1), #infectious
@@ -70,10 +70,10 @@ transitions = list(
   c(I31 = -1, R31 = +1), #Recovery
   c(S31 = +1), #Vaccinations
   c(R31 = +1),
-  c(S31 = -1), #Deaths
-  c(E31 = -1),
-  c(I31 = -1),
-  c(R31 = -1),
+  c(S31 = -1, S32 = +1), #aging
+  c(E31 = -1, E32 = +1),
+  c(I31 = -1, I32 = +1),
+  c(R31 = -1, R32 = +1),
   #old
   c(S32 = -1, E32 = +1), #Infection
   c(E32 = -1, I32 = +1), #infectious
@@ -85,12 +85,13 @@ transitions = list(
 )
 
 parameters = c(
-  R0 = c(16,14),
+  R0 = c(16,8),
   infectious.period = 7, #days
   latent.period = 8, #days
-  vacc.pro = c(0.9,0.75,0.6), #proportion
+  vacc.pro = c(0.6,0.6,0.6), #proportion
   birth.rate = 10, #per 1000, anum
-  death.rate = c(10,10) #per 1000, anum
+  young.size = 5, #years in the young division
+  death.rate = 10 #per 1000, anum
 )
 
 RateF <- function(x, p, t) {
@@ -101,8 +102,8 @@ RateF <- function(x, p, t) {
   f <- 1/p["latent.period"]
   gamma <- 1/p["infectious.period"]
   alpha <- p["birth.rate"]/365
-  omega <- c(p["death.rate1"]/365,
-             p["death.rate2"]/365)
+  omega <- p["death.rate"]/365
+  delta <- 1/(p["young.size"]*365)
   v1 <- ifelse(t<x["D1"], p["vacc.pro1"], 0)
   v2 <- ifelse(t<x["D2"], p["vacc.pro2"], 0)
   v3 <- ifelse(t<x["D3"], p["vacc.pro2"], 0)
@@ -137,19 +138,19 @@ RateF <- function(x, p, t) {
            x["I11"] * gamma,
            (N1.t/1000) * alpha * (1-v1) ,
            (N1.t/1000) * alpha * v1,
-           (x["S11"]/1000) * omega[1],
-           (x["E11"]/1000) * omega[1],
-           (x["I11"]/1000) * omega[1],
-           (x["R11"]/1000) * omega[1],
+           x["S11"] * delta,
+           x["E11"] * delta,
+           x["I11"] * delta,
+           x["R11"] * delta,
            x["S12"] * beta[1] * (I1/N.t) +  #old
            x["S12"] * beta[2] * (I2/N.t) +
            x["S12"] * beta[2] * (I3/N.t),
            x["E12"] * f,
            x["I12"] * gamma,
-           (x["S12"]/1000) * omega[2],
-           (x["E12"]/1000) * omega[2],
-           (x["I12"]/1000) * omega[2],
-           (x["R12"]/1000) * omega[2],
+           (x["S12"]/1000) * omega,
+           (x["E12"]/1000) * omega,
+           (x["I12"]/1000) * omega,
+           (x["R12"]/1000) * omega,
            #pop2
            x["S21"] * beta[1] * (I2/N.t) +  #young
            x["S21"] * beta[2] * (I1/N.t) +
@@ -158,19 +159,19 @@ RateF <- function(x, p, t) {
            x["I21"] * gamma,
            (N2.t/1000) * alpha * (1-v2) ,
            (N2.t/1000) * alpha * v2,
-           (x["S21"]/1000) * omega[1],
-           (x["E21"]/1000) * omega[1],
-           (x["I21"]/1000) * omega[1],
-           (x["R21"]/1000) * omega[1],
+           x["S21"] * delta,
+           x["E21"] * delta,
+           x["I21"] * delta,
+           x["R21"] * delta,
            x["S22"] * beta[1] * (I2/N.t) +  #old
            x["S22"] * beta[2] * (I1/N.t) +
            x["S22"] * beta[2] * (I3/N.t),
            x["E22"] * f,
            x["I22"] * gamma,
-           (x["S22"]/1000) * omega[2],
-           (x["E22"]/1000) * omega[2],
-           (x["I22"]/1000) * omega[2],
-           (x["R22"]/1000) * omega[2],
+           (x["S22"]/1000) * omega,
+           (x["E22"]/1000) * omega,
+           (x["I22"]/1000) * omega,
+           (x["R22"]/1000) * omega,
            #pop3
            x["S31"] * beta[1] * (I3/N.t) +  #young
            x["S31"] * beta[2] * (I1/N.t) +
@@ -179,29 +180,29 @@ RateF <- function(x, p, t) {
            x["I31"] * gamma,
            (N3.t/1000) * alpha * (1-v3) ,
            (N3.t/1000) * alpha * v3,
-           (x["S31"]/1000) * omega[1],
-           (x["E31"]/1000) * omega[1],
-           (x["I31"]/1000) * omega[1],
-           (x["R31"]/1000) * omega[1],
+           x["S31"] * delta,
+           x["E31"] * delta,
+           x["I31"] * delta,
+           x["R31"] * delta,
            x["S32"] * beta[1] * (I3/N.t) +  #old
            x["S32"] * beta[2] * (I1/N.t) +
            x["S32"] * beta[2] * (I2/N.t),
            x["E32"] * f,
            x["I32"] * gamma,
-           (x["S32"]/1000) * omega[2],
-           (x["E32"]/1000) * omega[2],
-           (x["I32"]/1000) * omega[2],
-           (x["R32"]/1000) * omega[2]
+           (x["S32"]/1000) * omega,
+           (x["E32"]/1000) * omega,
+           (x["I32"]/1000) * omega,
+           (x["R32"]/1000) * omega
   ))
  }
 
 #runs
 set.seed(100)
-runs=ssa.adaptivetau(init.values, transitions, RateF, parameters, tf=150)
+runs=ssa.adaptivetau(init.values, transitions, RateF, parameters, tf=3600)
 
 
 #Create summary measures for plotting
-runs <- cbind(runs, #store plot variables in a df
+runs <- cbind(runs,
 S = rowSums(runs[,c("S11","S12","S21","S22","S31","S32")]),
 I = rowSums(runs[,c("I11","I12","I21","I22","I31","I32")]),
 R = rowSums(runs[,c("R11","R12","R21","R22","R31","R32")]),
