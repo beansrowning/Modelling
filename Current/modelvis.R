5b514c9b00bc237b1b0c83d8f4c8898640c769d1
beansrowning <sean.browning94@gmail.com>
2017-03-17 00:08:58 +0000

    Still debugging
    1.mul_ins models no additional cases, which ins_1 does. Not sure which
    one to believe now...
    2.Performance is piss poor, need to parallelize

--- Current/modelvis.R
+++ Current/modelvis.R
@@ -134,7 +134,7 @@ batch_plot <- function(FUN = "mul_ins", batch = 100, fun_list = list(init.values
     #run a whole bunch of times and store into a df
     plot_dat <- data.frame(time = 0, I = 0, iter = 0)
     for(num in 1:batch){
-      SIR_r()
+      ins_1()
       run <- cbind(run, iter = num)
       plot_dat <- rbind(plot_dat,run)
     }
@@ -169,13 +169,13 @@ batch_plot <- function(FUN = "mul_ins", batch = 100, fun_list = list(init.values
   if(FUN == "mul_ins"){
     #throw some errors
     if(is.null(grp) == TRUE){
-        stop("No group specified!")
+        stop("No infection group specified!")
     }
     if(is.null(occ) == TRUE){
-       stop("No rate of insertion specified!")
+       stop("Number of insertions not specified!")
     }
-    if(is.null(insertion) == TRUE){
-        stop("No start time specified!")
+    if(is.null(insertion) == TRUE || insertion < 0){
+        stop("Something is wrong with your start time,partner.")
     }
 
     #multi-insertion function
@@ -183,35 +183,52 @@ batch_plot <- function(FUN = "mul_ins", batch = 100, fun_list = list(init.values
         P = fun_list[[4]], ins = occ, i_num = i_number,i_start = insertion,age = grp, tf = fun_list[[5]]
         ){
         inf_grp <- ifelse(age == "a","I2","I1")
-        
-        #define first run given time delay
+
+        #run given time delay
         if(i_start > 0){
             results <- ssa.adaptivetau(init,t,RF,P,i_start)
-            throw_2 <<- "this is working"
+            throw_2 <<- "starttime > 0"
+            for(i in 1:occ){
+                  results[nrow(results),inf_grp] = results[nrow(results),inf_grp] + i_num #add infected
+                  #accounting for time 0 starts
+                  init_new <- c(c(results[nrow(results),"S1"],results[nrow(results),"S2"]),
+                             c(results[nrow(results),"E1"],results[nrow(results),"E2"]),
+                             c(results[nrow(results),"I1"],results[nrow(results),"I2"]),
+                             c(results[nrow(results),"R1"],results[nrow(results),"R2"]),
+                             c(results[nrow(results),"D"]))
+                  t_new = ((tf-i_start)*(i/occ)-(tf-i_start)*((i-1)/occ))
+                  #more accounting for time 0 starts
+                  run = ssa.adaptivetau(init_new,t,RF,P,t_new)
+                  run = cbind(apply(run[,"time", drop=FALSE],2,function(x) x+results[nrow(results),"time"]),
+                    run[,-1]) #offset time by the final time of the past run
+                  results <- rbind(results,run[-1,]) #drop the first row
+              }
         }
-        #first run if no delay
+        #run if no delay
         if(i_start == 0){
             t_first = tf*(1/occ)
             init_new <- init
             init_new[inf_grp] = init[inf_grp] + i_num
             results <<- ssa.adaptivetau(init_new,t,RF,P,t_first)
-            throw_1 <<- "this shit is broken"
+            throw_1 <<- "starttime == 0"
+            #insertion loops
+            for(i in 1:(occ-1)){
+                  results[nrow(results),inf_grp] = results[nrow(results),inf_grp] + i_num #add infected
+                  #accounting for time 0 starts
+                  init_new <- c(c(results[nrow(results),"S1"],results[nrow(results),"S2"]),
+                             c(results[nrow(results),"E1"],results[nrow(results),"E2"]),
+                             c(results[nrow(results),"I1"],results[nrow(results),"I2"]),
+                             c(results[nrow(results),"R1"],results[nrow(results),"R2"]),
+                             c(results[nrow(results),"D"]))
+                  t_new = ((tf-i_start)*(i/occ)-(tf-i_start)*((i-1)/occ))
+                  #more accounting for time 0 starts
+                  run = ssa.adaptivetau(init_new,t,RF,P,t_new)
+                  run = cbind(apply(run[,"time", drop=FALSE],2,function(x) x+results[nrow(results),"time"]),
+                    run[,-1]) #offset time by the final time of the past run
+                  results <- rbind(results,run[-1,]) #drop the first row
+                }
         }
-        #loop insertion runs
-        for(i in 1:occ){
-              results[nrow(results),inf_grp] = results[nrow(results),inf_grp] + i_num #add infected
-              init_new <- c(c(results[nrow(results),"S1"],results[nrow(results),"S2"]),
-                         c(results[nrow(results),"E1"],results[nrow(results),"E2"]),
-                         c(results[nrow(results),"I1"],results[nrow(results),"I2"]),
-                         c(results[nrow(results),"R1"],results[nrow(results),"R2"]),
-                         c(results[nrow(results),"D"]))
-              t_new = i_start + ((tf-i_start)*(i/occ)-(tf-i_start)*((i-1)/occ)
-              #t_new = round((tf-i_start*(1/occ))) +results[nrow(results),"time"]
-              run = ssa.adaptivetau(init_new,t,RF,P,t_new)
-              run = cbind(apply(run[,"time", drop=FALSE],2,function(x) x+results[nrow(results),"time"]),
-                run[,-1]) #offset time by the final time of the past run
-              results <- rbind(results,run[-1,]) #drop the first row
-          }
+        #store results of run globally
         assign("results",results,envir=.GlobalEnv)
       }
 
@@ -223,24 +240,34 @@ batch_plot <- function(FUN = "mul_ins", batch = 100, fun_list = list(init.values
       results <- cbind(results,iter=i)
       plot_dat <- rbind(plot_dat,results[,c("time","I","iter"),drop=FALSE])
     }
+
+    #store plot data globally
     assign("plot_dat",plot_dat,envir=.GlobalEnv)
+    plot_dat$t_2 <- round(plot_dat$time,0)
+    #plot_dat_2 <- unique(plot_dat)
+
+    #Create summary measures from the runs
+    sum_dat <- ddply(plot_dat,.variables = "t_2",
+                     summarize,
+                     ave = mean(I),
+                     lb = quantile(I,0.25),
+                     ub = quantile(I,0.75))
+
     #graphing
-    #graph = ggplot(plot_dat)
-    #graph = graph + geom_point(aes(x=time, y=I), alpha=0.1, size=1)
+    graph = ggplot(plot_dat)
+    graph = graph + geom_point(aes(x=time, y=I), alpha=0.1, size=1)
     #graph = graph + geom_ribbon(data = sum_dat,
                   #aes(x=t_2,ymin=lb,ymax=ub),
                   #alpha = 0.25)
     #graph = graph + geom_line(data = sum_dat,
                 #aes(x=t_2,y=ave),
                 #size=0.5)
-    #graph = graph + labs(title= paste(batch,"SIR Iterations"),
-                         #x = "Time (days)",
-                         #y = "Infected (count)")
-    #graph = graph + theme_bw()
-    #plot(graph)
-    #assign("graph",graph,envir = .GlobalEnv) #for editing or saving
-
-
+    graph = graph + labs(title= paste(batch,"SIR Iterations"),
+                         x = "Time (days)",
+                         y = "Infected (count)")
+    graph = graph + theme_bw()
+    plot(graph)
+    assign("graph",graph,envir = .GlobalEnv) #for editing or saving
   }
   else{
     stop("I haven't coded for that option yet, you dunce!")
