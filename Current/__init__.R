6f9a4d31a294bfefe6b9f97d91519fe6f5db7c83
beansrowning <sean.browning94@gmail.com>
2017-06-28 18:16:12 -0500

    Made Init file
    Will load all dependencies and current functions

---
+++ Current/__init__.R
@@ -0,0 +1,20 @@
+# Modelling load file June 28 2017
+
+# Check and load dependencies
+dep <- list("adaptivetau", "Rcpp", "tidyverse", "parallel", "doParallel",
+            "foreach", "data.table")
+for (pkg in dep) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, dep = TRUE)
+    require(pkg, character.only = TRUE)
+  } else {
+    require(pkg, character.only = TRUE)
+  }
+}
+
+# Source data and functions
+files <- c("data.r", "Epi_detect.R", "multicore.R")
+sapply(files, source, .GlobalEnv)
+
+# Done.
+print("Done.")
