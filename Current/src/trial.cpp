f58a86e505b96341d0706219fb87af6238f92982
beansrowning <sean.browning94@gmail.com>
2017-07-11 16:01:21 -0500

    New Function - `trial.cpp` will eventually work as an optimization function to determine a set of parameters (insertion, i_number, and occ) that produce a model with an outbreak lasting at least 365 days. Still a work in progress to be certain.

---
+++ Current/src/trial.cpp
@@ -0,0 +1,78 @@
+#include <Rcpp.h>
+
+using namespace Rcpp;
+
+double roll(NumericVector set double oldval) {
+    double out, newval;
+    
+    if (!oldval)
+        out = R::sample(set, 1);
+        return out;
+    do {
+        newval = R::sample(set, 1);
+    } while (newval <= oldIns)
+    out = newVal;
+    return out;
+}
+
+// [[Rcpp::export]]
+NumericVector trialrun(Environment env String age) {
+    // Run batch run function and iterate
+    Environment global = Environment::global_env;
+    // NumericVector .inits = env["init.values"];
+    // NumericVector .params = env["parameters"];
+    // NumericVector .transitions = env["transitions"];
+    // Function .RateF = env["RateF"];
+    double tf = 3650;
+    // TODO: Create both of these functions
+    Function runBatch = global["brtrial"];
+    Function epiCheck = global["edtrial"]; 
+    
+    // Some parameter sets here
+    NumericVector infCount = R::seq(1,100);
+    NumericVector insertion = R::seq(0,3000);
+    NumericVector occ = R::seq(1,120);
+    double newCount, newIns, newOcc, oldCount, oldIns, oldOcc;
+    NumericVector out(2);
+    out.name = CharacterVector::create("Insertion Time", 
+                                       "Infected Count", 
+                                       "Occurances");
+    
+    bool successone;
+    oldIns, oldCount, oldOcc = 0;
+    do {
+        newIns = roll(insertion, oldIns);
+        newCount = roll(infCount, oldCount);
+        newOcc = oldOcc++;
+        // Rewrite whole thing?  
+        DataFrame run = runBatch(env, insertion = newIns, i_number = newCount, 
+                                 occ = newOcc, grp = age, length = tf); 
+        successone = epicheck(run);
+    } while (!successone);
+    
+    // First Sucesss
+    bool successtwo = 1;
+    out[0] = newIns;
+    out[1] = newCount;
+    out[2] = newOcc;
+    double oldCount = out[1]; 
+    // Now decrease number of people being inserted to optimize
+    do {
+        newCount = R::runif(min = 1, max = out[1]);
+        if (newCount >= oldCount)
+            continue;
+        DataFrame run = runBatch(env, insertion = out[0], i_number = newCount, 
+        occ = out[2], grp = age, length = tf); 
+        successtwo = epicheck(run);
+        if (successtwo)
+            oldCount = newCount;
+    } while (successtwo);
+    // Second Success 
+    // Store the minimized Count data in output vector
+    out[1] = newCount;
+    
+    
+    // Return output
+    
+    return out;
+}
