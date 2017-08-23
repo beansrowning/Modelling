dd9b90b926041432850f0cbe13aac472bb1ef2ce
beansrowning <lsh1604217@student.lshtm.ac.uk>
2017-08-23 18:52:54 +0100

    Debug

--- Current/Cluster/batchrun.sh
+++ Current/Cluster/batchrun.sh
@@ -4,4 +4,5 @@
 #$ -q parallel.q
 #$ -pe openmpi 16
 $ -R y
-mpirun -np 16 bash "~/R/x86_64-redhat-linux-gnu-library/3.4/snow/RMPISNOW" --no-save -q -f clusterrun.R
+export PATH = "home/lsh1604217/R/x86_64-redhat-linux-gnu-library/3.4/snow/RMPISNOW;%PATH%"
+mpirun -np 16 RMPISNOW --no-save -q -f clusterrun.R
