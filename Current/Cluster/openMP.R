c827d30c3d24aa7a6923b85a784be06cf852d472
beansrowning <sean.browning94@gmail.com>
2017-08-23 15:31:08 +0100

    dropped this )

--- Current/Cluster/openMP.R
+++ Current/Cluster/openMP.R
@@ -1,4 +1,4 @@
 #OPEN MP TEST
 require("parallel")
-print(paste0("Logical Cores Detected :", detectCores(logical = TRUE))
-print(paste0("Physical Cores Detected :", detectCores(logical = FALSE))
+print(paste0("Logical Cores Detected :", detectCores(logical = TRUE)))
+print(paste0("Physical Cores Detected :", detectCores(logical = FALSE)))
