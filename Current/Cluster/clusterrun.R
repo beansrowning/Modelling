03cca1999a1c4798ab8c474c260b9f3e10f983d8
beansrowning <sean.browning94@gmail.com>
2017-08-24 16:33:22 +0100

    Updated MPI variations to see how it will work in the cluster

--- Current/Cluster/clusterrun.R
+++ Current/Cluster/clusterrun.R
@@ -19,7 +19,8 @@ sourceCpp("../src/Croots.cpp")
 sourceCpp("../src/lenfind.cpp")
 # Source data and functions
 source("datap.r")
-source("solutionp.R")
+source("gridsearch1_mpi.R")
+source("../Parameterized/get_popvalues.R")
 print("All dependencies loaded.")
 
 
@@ -29,112 +30,29 @@ test <- foreach(i = 1:10) %dopar% {
         "of", mpi.comm.size())
 }
 
-solutions <- new.env()
-# Is this thing on?
-# Let's just check that MPI and foreach are playing nicely at all
-# before sending a large task
-solutions$t1 <- system.time(solutions$run_1 <- solutionSpace(swe,
-                                 insbound = c(0.01, 0.02),
-                                 vaccbound = c(0.90, 0.91),
-                                 len = 365))
-print("Run 1 finished")
-print(solutions$t1)
-# print(paste0("Starting Run 1 - ", date()))
-# # Run 1
-# # Sweden model
-# # ------------
-# # Old and young equally likely to be introduced
-# # length = 365 days
-# # end lag = 600 days
-# # Introduction rates : 0.01-0.1
-# # Vaccination rates  : 0.90-0.98
-# # Parameter space area : 9x10 = 90
-# solutions$t1 <- system.time(solutions$run_1 <- solutionSpace(swe,
-#                                 insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
-#                                               0.06, 0.07, 0.08, 0.09, 0.1),
-#                                 vaccbound = c(0.90, 0.91, 0.92, 0.93,
-#                                               0.94, 0.95, 0.96, 0.97, 0.98),
-#                                 len = 365))
-# print("Run 1 finished")
-# print(solutions$t1)
-# print(paste0("Starting Run 2 - ", date()))
-# # Run 2
-# # Sweden model
-# # ------------
-# # Old 1/2 as likely to be introduced
-# # length = 365 days
-# # end lag = 600 days
-# # Introduction rates : 0.01-0.1
-# # Vaccination rates  : 0.90-0.98
-# # Parameter space area : 9x10 = 90
-# solutions$t2 <- system.time(solutions$run_2 <- solutionSpace(swe,
-#                                   insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
-#                                               0.06, 0.07, 0.08, 0.09, 0.1),
-#                                   vaccbound = c(0.90, 0.91, 0.92, 0.93,
-#                                                 0.94, 0.95, 0.96, 0.97, 0.98),
-#                                   len = 365,
-#                                   grp = c(1,0.5)))
-# print("Run 2 finished")
-# print(solutions$t2)
-# print(paste0("Starting Run 3 - ", date()))
-# # Run 3
-# # Sweden model
-# # ------------
-# # young 1/2 as likely to be introduced
-# # length = 365 days
-# # end lag = 600 days
-# # Introduction rates : 0.01-0.1
-# # Vaccination rates  : 0.90-0.98
-# # Parameter space area : 9x10 = 90
-# solutions$t3 <- system.time(solutions$run_3 <- solutionSpace(swe,
-#                                 insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
-#                                               0.06, 0.07, 0.08, 0.09, 0.1),
-#                                 vaccbound = c(0.90, 0.91, 0.92, 0.93,
-#                                               0.94, 0.95, 0.96, 0.97, 0.98),
-#                                 len = 365,
-#                                 grp = c(0.5,1)))
-# print("Run 3 finished")
-# print(solutions$t3)
-# print(paste0("Starting Run 4 - ", date()))
-# # Run 4
-# # Sweden model
-# # ------------
-# # only young introduced
-# # length = 365 days
-# # end lag = 600 days
-# # Introduction rates : 0.01-0.1
-# # Vaccination rates  : 0.90-0.98
-# # Parameter space area : 9x10 = 90
-# solutions$t4 <- system.time(solutions$run_4 <- solutionSpace(swe,
-#                                   insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
-#                                               0.06, 0.07, 0.08, 0.09, 0.1),
-#                                   vaccbound = c(0.90, 0.91, 0.92, 0.93,
-#                                                 0.94, 0.95, 0.96, 0.97, 0.98),
-#                                   len = 365,
-#                                   grp = c(1,0)))
-# print("Run 4 finished")
-# print(solutions$t4)
-# print(paste0("Starting Run 5 - ", date()))
+measles_land$parameters["start.time"] <- 0
+set.seed(1000)
+# Run 2
+#-------
+# Testing initial population size and effective vaccination rate on outbreak length
+# Population sizes : 300,000 - 500,000 by 10,000
+# Vaccination rates : 0.90 - 1 by 0.01
+# Baseline Seroprevalence : 92%
+# Case introduction rate : 0.01 (approximately 1 per 100 days)
+# Equal introduction likelihood in either group
+# Total grid area: 21 x 11 = 231
+print(paste0("Begining Run 2 - ", date()))
+measles_land$parameters["introduction.rate"] <- 0.01
+measles_land$t2 <- system.time(measles_land$run_2 <- solutionSpace(measles_land,
+                                    insbound = seq(300000, 500000, 10000),
+                                    vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
+                                                  0.95, 0.96, 0.97, 0.98, 0.99, 1),
+                                    len = 365,
+                                    # let's try 800 again
+                                    offset = 800,
+                                    sero.p = c(0.92, 0.92))
+print(paste0("Run 2 done - ", measles_land$t2))
 
-# # Run 5
-# # Sweden model
-# # ------------
-# # only old introduced
-# # length = 365 days
-# # end lag = 600 days
-# # Introduction rates : 0.01-0.1
-# # Vaccination rates  : 0.90-0.98
-# # Parameter space area : 9x10 = 90
-# solutions$t5 <- system.time(solutions$run_5 <- solutionSpace(swe,
-#                                 insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
-#                                               0.06, 0.07, 0.08, 0.09, 0.1),
-#                                 vaccbound = c(0.90, 0.91, 0.92, 0.93,
-#                                               0.94, 0.95, 0.96, 0.97, 0.98),
-#                                 len = 365,
-#                                 grp = c(0,1)))
-# print("Run 5 finished")
-# print(solutions$t5)
-# print(paste0("Saving - ", date()))
 save(solutions, file = "hpcrun228.dat")
 print(paste0("Done. - ", date()))
 closeCluster(cl)
--- Current/Cluster/clusterrun.sh
+++ Current/Cluster/clusterrun.sh
@@ -2,6 +2,7 @@
 #$ -cwd -V
 #$ -l mem_free=1G,h_vmem=1G
 #$ -q parallel.q
-#$ -pe openmpi 16
+#$ -pe openmpi 17
 #$ -R y
-mpirun -np 16 R --no-save -q -f clusterrun.R
+# 1 + number of slaves
+mpirun -np 17 R --no-save -q -f clusterrun.R
