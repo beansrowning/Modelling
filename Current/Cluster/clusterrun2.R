6035c740a72ba7c53aa3ac07b5dab7a1a429db7a
beansrowning <sean.browning94@gmail.com>
2017-08-24 22:56:18 +0100

    Two more ready to load

---
+++ Current/Cluster/clusterrun2.R
@@ -0,0 +1,58 @@
+require(doMPI)
+# Make Cluster
+cl <- startMPIcluster(16, comm = 1)
+registerDoMPI(cl)
+clusterSize(cl)
+print(cl)
+require(Rcpp)
+require(adaptivetau)
+require(parallel)
+require(foreach)
+require(iterators)
+require(data.table)
+
+cs <- list(chunkSize = ceiling(10000/(clusterSize(cl) - 1)))
+sourceCpp("../src/Croots.cpp")
+sourceCpp("../src/lenfind.cpp")
+# Source data and functions
+source("data.r")
+source("gridsearch1_mpi.R")
+source("../Parameterized/get_popvalues.R")
+print("All dependencies loaded.")
+
+
+set.seed(1000)
+# Test
+foreach(i = 1:mpi.comm.size()) %dopar% {
+  paste(Sys.info()[["nodename"]], Sys.getpid(), mpi.comm.rank(),
+        "of", mpi.comm.size())
+}
+
+measles_land$parameters["start.time"] <- 0
+set.seed(1000)
+# Run 4
+#-------
+# Testing initial population size and effective vaccination rate on outbreak length
+# Population sizes : 300,000 - 500,000 by 10,000
+# Vaccination rates : 0.90 - 1 by 0.01
+# Baseline Seroprevalence : 94%
+# Case introduction rate : 0.01 (approximately 1 per 100 days)
+# Equal introduction likelihood in either group
+# Total grid area: 21 x 11 = 231
+# Will adjust depth accordingly
+print(paste0("Begining Run 3 - ", date()))
+measles_land$parameters["introduction.rate"] <- 0.01
+measles_land$t4 <- system.time(measles_land$run_4 <- solutionSpace(measles_land,
+                                    insbound = seq(300000, 500000, 10000),
+                                    vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
+                                                  0.95, 0.96, 0.97, 0.98, 0.99, 1),
+                                    len = 365,
+                                    # no dice
+                                    offset = 1200,
+                                    sero.p = c(0.94, 0.94)))
+print(paste0("Run 4 done - ", measles_land$t4))
+
+save(measles_land, file = "../../Data/hpc_3_94.dat")
+print(paste0("Done. - ", date()))
+closeCluster(cl)
+mpi.quit()
--- Current/Parameterized/part1.R
+++ Current/Parameterized/part1.R
@@ -422,4 +422,16 @@ save(measles_land, file="../../Data/gridsearch_part1.dat")
 # need to have some measure of the probability that this occurs (p value, etc).
 
 
+# By the end of the night, I should have a dat file with the measles_land environment
+# along with the following models :
+# 1. Run 1 - 0.90% seroprevalence - In progress (LG30 PC1) (2000 sims)
+# 2. Run 2 - 0.92% seroprevalence - In progress (LG30 PC2) (2000 sims)
+# 3. Run 3 - 0.94% seroprevalence - In progress (cluster 2)
+# 3. Run 3 - 0.95% seroprevalence - In progress (cluster 1)
+# 4. Run 4 - 0.96% seroprevalence - Done (run 3)
+
+
+
+
+
 
