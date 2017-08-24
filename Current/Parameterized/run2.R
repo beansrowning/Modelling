b56b0feeb064157d123967e5108ef3bd08670b0b
beansrowning <lsh1604217@student.lshtm.ac.uk>
2017-08-24 21:20:38 +0100

    Updated again

--- Current/Parameterized/run2.R
+++ Current/Parameterized/run2.R
@@ -93,6 +93,7 @@ require(foreach)
 load("../../Data/gridsearch1_part2.dat")
 source("gridsearch1.R")
 source("get_popvalues.R")
+source("pot.R")
 set.seed(1000)
 # Run 3
 #-------
@@ -103,13 +104,13 @@ set.seed(1000)
 # Case introduction rate : 0.01 (approximately 1 per 100 days)
 # Equal introduction likelihood in either group
 # Total grid area: 21 x 11 = 231
-# Trying reduced depth : 5000 (simulations way too long to start at 10K)
+# Trying reduced depth : 2000 (simulations way too long to start at 10K)
 print(paste0("Begining Run 1 - ", date()))
 measles_land$parameters["introduction.rate"] <- 0.01
 measles_land$parameters["start.time"] <- 0
 measles_land$parameters["end.time"] <- 365
 measles_land$t1 <- system.time(measles_land$run_1 <- solutionSpace(measles_land,
-                                    count = 5000,
+                                    count = 2000,
                                     insbound = seq(300000, 500000, 10000),
                                     vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
                                                   0.95, 0.96, 0.97, 0.98, 0.99, 1),
@@ -117,7 +118,17 @@ measles_land$t1 <- system.time(measles_land$run_1 <- solutionSpace(measles_land,
                                     offset = 1200,
                                     sero.p = c(0.90, 0.90)))
 print(paste0("Run 1 done - ", measles_land$t1))
-
+threedPlot(measles_land,
+           measles_land$run_1,
+           c("max", "median"),
+           xlab = "Population Size",
+           ylab = "MMR Vaccination Rate (%)",
+           zlab = "Outbreak Length (days)",
+           title = "Grid Search Run 1 : 90% Seroprevalence")
+measles_land$p1 <- measles_land$plot
+measles_land$p_d1 <- measles_land$plot_dat
+rm(plot_dat, enivr = measles_land)
+rm(plot, enivr = measles_land)
 # let's save our progress and be done
 print(paste0("All Done! - ", date()))
 save(measles_land, file="../../Data/gridsearch1_part2.dat")
