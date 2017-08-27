2d9c0171aaa369559f4db1b000841ba6bde57b84
beansrowning <sean.browning94@gmail.com>
2017-08-27 16:00:16 +0100

    Debug

--- Current/Cluster/mpitest.R
+++ Current/Cluster/mpitest.R
@@ -1,13 +1,13 @@
 # require(Rmpi)
 require(doMPI)
 # Spawn R Slaves
-universe <- 13
-n <- (universe - 1) / 3
-mpi.spawn.Rslaves(n)
+# universe <- 13
+# n <- (universe - 1) / 3
+mpi.spawn.Rslaves(4)
 mpi.remote.exec(print(mpi.comm.rank()))
 
 #---Define the number of workers we will assign to each slave----
-m <- n / 2
+m <- 2
 #---Define Slave function-----------------
 slavedoWorkers <- function(x) {
   require(doMPI)
