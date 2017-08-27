b358c2713e2ade40e59adf8fa73ee512a0d1acec
beansrowning <sean.browning94@gmail.com>
2017-08-27 15:03:30 +0100

    Debug

--- Current/Cluster/mpitest.R
+++ Current/Cluster/mpitest.R
@@ -1,20 +1,22 @@
 # require(Rmpi)
 require(doMPI)
 # Make Cluster
-cl <- startMPIcluster(4, comm = 0)
+cl1 <- startMPIcluster(4, comm = 1)
 registerDoMPI(cl)
 require(parallel)
 require(foreach)
 require(iterators)
-
+print(cl)
 set.seed(1000)
 foreach(i = 1:(getDoParWorkers())) %dopar% {
   #---Report primary node----------------------
-  cat(Sys.info()[["nodename"]], "\n")
+  cat(Sys.info()[["nodename"]],Sys.getpid(), 
+      mpi.comm.rank(), mpi.comm.size() "\n")
   #---Assign slaves to primary node------------
-  cluster <- startMPIcluster(2, comm = i)
-  assign(paste0("cl",i),cluster, envir = environment())
-  registerDoMPI(paste0("cl",i))
+  cluster <- startMPIcluster(2, comm = (i+1))
+  assign(paste0("cl", i), cluster, envir = environment())
+  registerDoMPI(paste0("cl", i))
+  cat("DoParWorkers : "getDoParWorkers(), "\n")
   #---Have those slaves report themselves-------
   foreach(j = 1:(goDoParWorkers())) %dopar% {
     paste(Sys.getpid(), mpi.comm.rank(), "of", mpi.comm.size())
