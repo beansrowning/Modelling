2d8b8bb5a696a83838ada49bac108accc7a4c450
beansrowning <sean.browning94@gmail.com>
2017-08-28 23:41:54 +0100

    Almost missed that - Run 3 was run as c(1, 5) not as c(1, 0.5)

--- Current/Cluster/clusterrun2.R
+++ Current/Cluster/clusterrun2.R
@@ -52,37 +52,37 @@ set.seed(1000)
 # Case introduction over the course of 1 year
 # Offest by 2000 days beyond
 solutions <- new.env()
-print(paste0("Begining Run 1 - ", date()))
-solutions$t1 <- system.time(solutions$run_1 <- solutionSpace(swe,
-                                    insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
-                                                 0.06, 0.07, 0.08, 0.09, 0.1),
-                                    vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
-                                                  0.95, 0.96, 0.97, 0.98, 0.99, 1),
-                                    len = 365,
-                                    offset = 2000))
-print(paste0("Run 1 done - ", solutions$t1[3]))
-save(solutions, file = "../../Data/gridsearch2.dat")
+# print(paste0("Begining Run 1 - ", date()))
+# solutions$t1 <- system.time(solutions$run_1 <- solutionSpace(swe,
+#                                     insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
+#                                                 0.06, 0.07, 0.08, 0.09, 0.1),
+#                                     vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
+#                                                   0.95, 0.96, 0.97, 0.98, 0.99, 1),
+#                                     len = 365,
+#                                     offset = 2000))
+# print(paste0("Run 1 done - ", solutions$t1[3]))
+# save(solutions, file = "../../Data/gridsearch2.dat")
 
-# Run 2
-# Insertion rates :   0.01-0.1 
-# vaccinations rates: 0.9-1
-# Total grid size : 10 * 11 = 110
-# Search depth : 10,000 runs
-# All other population values fixed
-# Old persons twice as likely to be introduced
-# Case introduction over the course of 1 year
-# Offest by 2000 days beyond
-print(paste0("Begining Run 2 - ", date()))
-solutions$t2 <- system.time(solutions$run_2 <- solutionSpace(swe,
-                                    insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
-                                                 0.06, 0.07, 0.08, 0.09, 0.1),
-                                    vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
-                                                  0.95, 0.96, 0.97, 0.98, 0.99, 1),
-                                    len = 365,
-                                    grp = c(0.5, 1),
-                                    offset = 2000))
-print(paste0("Run 2 done - ", solutions$t2[3]))
-save(solutions, file = "../../Data/gridsearch2.dat")
+# # Run 2
+# # Insertion rates :   0.01-0.1 
+# # vaccinations rates: 0.9-1
+# # Total grid size : 10 * 11 = 110
+# # Search depth : 10,000 runs
+# # All other population values fixed
+# # Old persons twice as likely to be introduced
+# # Case introduction over the course of 1 year
+# # Offest by 2000 days beyond
+# print(paste0("Begining Run 2 - ", date()))
+# solutions$t2 <- system.time(solutions$run_2 <- solutionSpace(swe,
+#                                     insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
+#                                                 0.06, 0.07, 0.08, 0.09, 0.1),
+#                                     vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
+#                                                   0.95, 0.96, 0.97, 0.98, 0.99, 1),
+#                                     len = 365,
+#                                     grp = c(0.5, 1),
+#                                     offset = 2000))
+# print(paste0("Run 2 done - ", solutions$t2[3]))
+# save(solutions, file = "../../Data/gridsearch2.dat")
 
 # Run 3
 # Insertion rates :   0.01-0.1 
@@ -100,10 +100,10 @@ solutions$t3 <- system.time(solutions$run_3 <- solutionSpace(swe,
                                     vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
                                                   0.95, 0.96, 0.97, 0.98, 0.99, 1),
                                     len = 365,
-                                    grp = c(1, 05),
+                                    grp = c(1, 0.5),
                                     offset = 2000))
 print(paste0("Run 2 done - ", solutions$t2[3]))
-save(solutions, file = "../../Data/gridsearch2.dat")
+save(solutions, file = "../../Data/swe3.dat")
 
 print(paste0("Done. - ", date()))
 closeCluster(cl)
