d14d79289912c6aefec533767b8bee74ac4c397a
beansrowning <sean.browning94@gmail.com>
2017-08-29 18:35:42 +0100

    Changed

--- Current/Parameterized/gcrun.sh
+++ Current/Parameterized/gcrun.sh
@@ -5,7 +5,7 @@ export PATH=~/R/R-3.4.1/bin:$PATH
 git pull
 R --vanilla -f latvia.R
 R --vanilla -f malta.R
-git add ../../Data/malta.dat
-git add ../../Data/latvia.dat
+git add ../../Data/malta2.dat
+git add ../../Data/latvia2.dat
 git commit -m"Malta & Latvia runs 2 done"
 gcloud compute instances stop debian-1 --zone=europe-west1-b
