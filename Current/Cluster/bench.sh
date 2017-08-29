1ab83d608d4e283aae48ba023602715f0f89c749
beansrowning <sean.browning94@gmail.com>
2017-08-29 15:18:24 +0100

    Let's try this

--- Current/Cluster/bench.sh
+++ Current/Cluster/bench.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #$ -cwd -V
+#$ -M lsh1604217@student.lshtm.ac.uk
+#$ -m n
 #$ -l mem_free=1G,h_vmem=1G
 #$ -q parallel.q
-#$ -pe smp 8
+#$ -pe smp 12
 #$ -R y
-R --vanilla -f ../gsbench.R
+R CMD BATCH ../gsbench.R
