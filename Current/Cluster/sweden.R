6641c0cef162aedd7e6ca2639ee0a5296addba39
beansrowning <sean.browning94@gmail.com>
2017-08-29 20:40:09 +0100

    All 3 Ready to go in the Cluster

---
+++ Current/Cluster/sweden.R
@@ -0,0 +1,88 @@
+require(doMPI)
+# Make Cluster
+cl <- startMPIcluster()
+registerDoMPI(cl)
+require(Rcpp)
+require(data.table)
+require(adaptivetau)
+source("gridsearch2_mpi.R")
+initEnvir <- function() {
+  library(adaptivetau)
+  len <- get("len", parent.frame())
+}
+opts <- list(chunkSize = ceiling(10000/(getDoParWorkers())), 
+initEnvir = initEnvir)
+
+solutions <- new.env()
+# Run 1 - 12 month delay
+# Insertion rates :   0.01-0.1 
+# vaccinations rates: 0.9-1
+# Total grid size : 10 * 11 = 110
+# Search depth : 10,000 runs
+# All other population values fixed
+# Case introduction over the course of 1 year
+# Offest by 2000 days beyond
+print(paste0("Begining Run 1 - ", date()))
+# Start time at 12 mo
+swe$parameters["start.time"] <- 365
+solutions$t1 <- system.time(solutions$run_1 <- solutionSpace(swe,
+                                    insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
+                                                 0.06, 0.07, 0.08, 0.09, 0.1),
+                                    vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
+                                                  0.95, 0.96, 0.97, 0.98, 0.99, 1),
+                                    # Len is start.time + insertion time 
+                                    len = 730,
+                                    # Offset is appended at the end of insertion
+                                    offset = 2000))
+print(paste0("Run 1 done - ", solutions$t1[3]))
+save(solutions, file = "../../Data/sweden_2.dat")
+
+# Run 2 - 24 month delay
+# Insertion rates :   0.01-0.1 
+# vaccinations rates: 0.9-1
+# Total grid size : 10 * 11 = 110
+# Search depth : 10,000 runs
+# All other population values fixed
+# Case introduction over the course of 1 year
+# Offest by 2000 days beyond
+print(paste0("Begining Run 2 - ", date()))
+# Start time at 24 mo
+swe$parameters["start.time"] <- 730
+solutions$t2 <- system.time(solutions$run_2 <- solutionSpace(swe,
+                                    insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
+                                                 0.06, 0.07, 0.08, 0.09, 0.1),
+                                    vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
+                                                  0.95, 0.96, 0.97, 0.98, 0.99, 1),
+                                    # Len is start.time + insertion time 
+                                    len = 1095,
+                                    # Offset is appended at the end of insertion
+                                    offset = 2000))
+print(paste0("Run 2 done - ", solutions$t2[3]))
+save(solutions, file = "../../Data/sweden_2.dat")
+
+# Run 3 - 36 month delay
+# Insertion rates :   0.01-0.1 
+# vaccinations rates: 0.9-1
+# Total grid size : 10 * 11 = 110
+# Search depth : 10,000 runs
+# All other population values fixed
+# Case introduction over the course of 1 year
+# Offest by 2000 days beyond
+source("../../Data/model_global.R")
+print(paste0("Begining Run 3 - ", date()))
+# Start time at 36 mo
+swe$parameters["start.time"] <- 1095
+solutions$t3 <- system.time(solutions$run_3 <- solutionSpace(swe,
+                                    insbound = c(0.01, 0.02, 0.03, 0.04, 0.05,
+                                                 0.06, 0.07, 0.08, 0.09, 0.1),
+                                    vaccbound = c(0.9, 0.91, 0.92, 0.93, 0.94,
+                                                  0.95, 0.96, 0.97, 0.98, 0.99, 1),
+                                    # Len is start.time + insertion time 
+                                    len = 1460,
+                                    # Offset is appended at the end of insertion
+                                    offset = 2000))
+print(paste0("Run 3 done - ", solutions$t3[3]))
+save(solutions, file = "../../Data/sweden_2.dat")
+
+closeCluster(cl)
+mpi.quit()
