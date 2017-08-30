e06e5f43c297d9376164bc2be95f92b1b7be3b32
beansrowning <lsh1604217@student.lshtm.ac.uk>
2017-08-30 13:14:56 +0100

    Gotta Go Fast

---
+++ Current/Cluster/maltapt1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#$ -N Malta_1
+#$ -cwd -V
+#$ -M lsh1604217@student.lshtm.ac.uk
+#$ -m e
+#$ -l mem_free=1G,h_vmem=1G
+#$ -q parallel.q
+#$ -pe openmpi 32
+#$ -R y
+mpirun -np 32 R CMD BATCH maltapt1.R
