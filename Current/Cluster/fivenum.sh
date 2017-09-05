a1d7332bdac9ce63c8c552b5bb3bdf76a9293a2a
beansrowning <lsh1604217@student.lshtm.ac.uk>
2017-09-05 02:43:23 +0100

    pull

--- Current/Cluster/fivenum.sh
+++ Current/Cluster/fivenum.sh
@@ -3,8 +3,8 @@
 #$ -cwd -V
 #$ -M lsh1604217@student.lshtm.ac.uk
 #$ -m e
-#$ -l mem_free=1G,h_vmem=1G
+#$ -l mem_free=1.5G,h_vmem=1.5G
 #$ -q parallel.q
-#$ -pe openmpi 64
+#$ -pe openmpi 128
 #$ -R y
-mpirun -np 64 R CMD BATCH fivenum.R
+mpirun -np 128 R CMD BATCH fivenum.R
